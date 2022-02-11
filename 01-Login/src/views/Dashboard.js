d5af09446267724fcbb28a20bd0b734ef1984015
Chris Loper <chrisloper@utexas.edu>
2022-02-11 10:49:08 -0600

    change jsx in Dashboard.js

--- 01-Login/src/views/Dashboard.js
+++ 01-Login/src/views/Dashboard.js
@@ -7,28 +7,28 @@ function Dashboard() {
     if (isLoading) {
         return null;
     }
-    if (isAuthorized) {
-        return (
-            <div className={styles.dashboard}>
-                <div className={styles.title}>Welcome!</div>
-                <div className={styles.message}>
-                    You successfully signed in with Passage.
-                    <br/><br/>
-                    Your email is: <b>{username}</b>
-                </div>
-            </div>
-        )
-    }
+    const authorizedBody = 
+    <>
+        You successfully signed in with Passage.
+        <br/><br/>
+        Your email is: <b>{username}</b>
+    </>
+
+    const unauthorizedBody = 
+    <>
+        You have not logged in and cannot view the dashboard.
+        <br/><br/>
+        <a href="/" className={styles.link}>Login to continue.</a>
+    </>
+
     return (
         <div className={styles.dashboard}>
-            <div className={styles.title}>Unauthorized</div>
+            <div className={styles.title}>{isAuthorized ? 'Welcome!' : 'Unauthorized'}</div>
             <div className={styles.message}>
-                You have not logged in and cannot view the dashboard.
-                <br/><br/>
-                <a href="/" className={styles.link}>Login to continue.</a>
+                { isAuthorized ? authorizedBody : unauthorizedBody }
             </div>
         </div>
-    ); 
+    );
 
 }
 
