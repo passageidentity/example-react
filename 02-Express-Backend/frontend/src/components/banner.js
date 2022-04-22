adc097d878271b7971bd9efcb382e1fba6ee17b8
Kevin Flanagan <90063911+flanagankp@users.noreply.github.com>
2022-04-22 12:55:15 -0500

    update examples with new brand (#18)

--- 02-Express-Backend/frontend/src/components/banner.js
+++ 02-Express-Backend/frontend/src/components/banner.js
@@ -6,7 +6,7 @@ function Banner() {
             <a href="https://passage.id/" ><div className={styles.passageLogo}></div></a>
             <div className={styles.headerText}>Passage + React Example App</div>
             <div className={styles.spacer}></div>
-            <a href="https://passage.id/" ><span className={styles.text}>Go to Passage</span></a>
+            <a href="https://passage.id/" className={styles.link}>Go to Passage</a>
         </div>
     );
 }
